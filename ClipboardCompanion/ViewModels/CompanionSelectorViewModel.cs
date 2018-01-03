d3cd207dc5e4a62684c3c47b3a82b422f1255a68
Keith Ott <ducttape12@yahoo.com>
2018-01-03 15:23:10 -0600

    Building CompanionSelector and ViewModel

---
+++ ClipboardCompanion/ViewModels/CompanionSelectorViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using ClipboardCompanion.Services;
+using ClipboardCompanion.Views;
+
+namespace ClipboardCompanion.ViewModels
+{
+    public class CompanionSelectorViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<BaseCompanionControl> Companions { get; } = new ObservableCollection<BaseCompanionControl>();
+
+        private BaseCompanionControl _selectedCompanionControl;
+
+        public BaseCompanionControl SelectedCompanionControl
+        {
+            get => _selectedCompanionControl;
+            set
+            {
+                _selectedCompanionControl = value;
+                OnPropertyChanged(nameof(SelectedCompanionControl));
+            }
+        }
+
+        public CompanionSelectorViewModel()
+        {
+            Companions.Add(new GuidCreatorControl(new GuidCreatorCompanionViewModel(null, null, null)));
+        }
+
+        //public CompanionSelectorViewModel(GuidCreatorControl guidCreatorControl,
+        //    TextCleanerCompanionControl textCleanerCompanionControl)
+        //{
+        //    Companions.Add(guidCreatorControl);
+        //    Companions.Add(textCleanerCompanionControl);
+        //}
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
