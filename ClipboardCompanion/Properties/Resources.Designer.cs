2d1a097e41632d8e1cf7e99c8326aaaf7c042754
Keith Ott <ducttape12@yahoo.com>
2018-01-16 14:51:17 -0600

    Some initial cleanup on About dialog

--- ClipboardCompanion/Properties/Resources.Designer.cs
+++ ClipboardCompanion/Properties/Resources.Designer.cs
@@ -91,15 +91,10 @@ namespace ClipboardCompanion.Properties {
         /// <summary>
         ///   Looks up a localized string similar to Copyright (C) 2018  Keith Ott
         ///
-        ///This program is free software: you can redistribute it and/or modify
-        ///it under the terms of the GNU General Public License as published by
-        ///the Free Software Foundation, either version 3 of the License, or
-        ///(at your option) any later version.
+        ///This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
         ///
-        ///This program is distributed in the hope that it will be useful,
-        ///but WITHOUT ANY WARRANTY; without even the implied warranty of
-        ///MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-        ///GNU General Public License for more detai [rest of string was truncated]&quot;;.
+        ///This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+        ///        /// [rest of string was truncated]&quot;;.
         /// </summary>
         public static string License {
             get {
